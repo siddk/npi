dc49ee4186af8a0f5223eeee805da6b5e7eb911f
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-12-24 19:07:20 -0800

    Add NPI Core LSTM logic

--- model/npi.py
+++ model/npi.py
@@ -27,10 +27,12 @@ class NPI():
         self.reset_state()
         self.h = self.npi_core()
 
+
+
     def reset_state(self):
         """
         Zero NPI Core LSTM Hidden States. LSTM States are represented as a Tuple, consisting of the
-        LSTM C State, and the LSTM H State.
+        LSTM C State, and the LSTM H State (in that order: (c, h)).
         """
         zero_state = np.zeros((self.bsz, self.npi_core_dim))
         self.h_states = [(zero_state, zero_state) for _ in range(self.npi_core_layers)]
@@ -39,7 +41,22 @@ class NPI():
         """
         Build the NPI LSTM core, feeding the program embedding and state encoding to a multi-layered
         LSTM, returning the h-state of the final LSTM layer.
+
+        References: Reed, de Freitas [2]
         """
-        state_in = self.state_dim         # Shape: [bsz,
+        s_in = self.state_dim                                    # Shape: [bsz, state_dim]
+        p_in = self.program_embedding                            # Shape: [bsz, 1, program_dim]
 
+        # Reshape state_in
+        s_in = tflearn.reshape(s_in, [None, 1, self.state_dim])  # Shape: [bsz, 1, state_dim]
 
+        # Concatenate s_in, p_in
+        c = tflearn.merge([s_in, p_in], 'concat', axis=2)        # Shape: [bsz, 1, state + prog]
+
+        # Feed through Multi-Layer LSTM
+        for i in range(len(self.npi_core_layers)):
+            c, self.h_states[i] = tflearn.lstm(c, self.npi_core_dim, return_seq=True,
+                                               initial_state=self.h_states[i], return_states=True)
+
+        # Return Top-Most LSTM H-State
+        return self.h_states[-1][1]                              # Shape: [bsz, npi_core_dim]
